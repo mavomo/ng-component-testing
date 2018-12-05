0aeec23366dae65c68005ca29f2ceb1f4d26337a
Michelle AVOMO <michelle.avomo@partnre.com>
2018-12-05 11:09:57 +0100

    starting deep IT tests for heroesComponent

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -1,10 +1,11 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {HeroesComponent} from './heroes.component';
-import {Component, Input} from '@angular/core';
+import {Component, Input, NO_ERRORS_SCHEMA} from '@angular/core';
 import {HeroService} from '../hero.service';
 import {of} from 'rxjs';
 import {Hero} from '../hero';
 import {By} from '@angular/platform-browser';
+import {HeroComponent} from '../hero/hero.component';
 
 describe('Heroes component should', () => {
 
@@ -26,36 +27,62 @@ describe('Heroes component should', () => {
     hero: Hero;
   }
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        HeroesComponent,
-        FakeHeroComponent],
-      providers: [
-        {provide: HeroService, useValue: heroServiceMock}
-      ]
+  describe('run *swallow* integration tests', () => {
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        declarations: [
+          HeroesComponent,
+          FakeHeroComponent],
+        providers: [
+          {provide: HeroService, useValue: heroServiceMock}
+        ]
+      });
+      fixture = TestBed.createComponent(HeroesComponent);
+      heroesComponent = fixture.componentInstance;
     });
-    fixture = TestBed.createComponent(HeroesComponent);
-    heroesComponent = fixture.componentInstance;
+    it('create a component', () => {
+      expect(heroesComponent).toBeTruthy('component should be intialized');
+    });
+
+    it('set the heroes correctly from the service', () => {
+      heroServiceMock.getHeroes.and.returnValues(of(HEROES));
+      fixture.detectChanges();
+      expect(heroesComponent.heroes).toEqual(HEROES, 'heroes should be set in the component');
+    });
+
+    it('bind the heroes to the DOM as an ordered list', () => {
+      heroServiceMock.getHeroes.and.returnValues(of(HEROES));
+      fixture.detectChanges();
+
+      const liTag = fixture.debugElement.queryAll(By.css('li'));
+
+      expect(liTag.length).toEqual(3, 'number of heroes should be 3');
+    });
+
   });
 
-  it('create a component', () => {
-    expect(heroesComponent).toBeTruthy('component should be intialized');
-  });
+  describe('run *deep integration tests*', () => {
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        declarations: [HeroesComponent, HeroComponent],
+        providers: [
+          {provide: HeroService, useValue: heroServiceMock}
+        ],
+        schemas:[NO_ERRORS_SCHEMA]
+      });
+      fixture = TestBed.createComponent(HeroesComponent);
+      heroesComponent = fixture.componentInstance;
+      });
 
-  it('set the heroes correctly from the service', () => {
-    heroServiceMock.getHeroes.and.returnValues(of(HEROES));
-    fixture.detectChanges();
-    expect(heroesComponent.heroes).toEqual(HEROES, 'heroes should be set in the component');
-  });
+    it('render each hero as HeroComponent', () => {
+      heroServiceMock.getHeroes.and.returnValues(of(HEROES));
 
-  it('bind the heroes to the DOM as an ordered list', () => {
-    heroServiceMock.getHeroes.and.returnValues(of(HEROES));
-    fixture.detectChanges();
+      fixture.detectChanges();
 
-    const liTag = fixture.debugElement.queryAll(By.css('li'));
+      const heroComponentDEs = fixture.debugElement.queryAll((By.directive(HeroComponent)));
 
-    expect(liTag.length).toEqual(3, 'number of heroes should be 3');
+      expect(heroComponentDEs.length).toBe(3, 'should containt 3 li nodes');
+    });
   });
 
 });
