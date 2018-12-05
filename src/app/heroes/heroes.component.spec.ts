7a8d05a224f9e12bf2a6397afff819792525ae6e
Michelle AVOMO <michelle.avomo@partnre.com>
2018-12-05 10:35:15 +0100

    adding fake heroComponent instead of the NO_ERROR_SCHEMA

---
+++ src/app/heroes/heroes.component.spec.ts
@@ -0,0 +1,51 @@
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {HeroesComponent} from './heroes.component';
+import {Component, Input} from '@angular/core';
+import {HeroService} from '../hero.service';
+import {of} from 'rxjs';
+import {Hero} from '../hero';
+
+describe('Heroes component should', () => {
+
+  let fixture: ComponentFixture<HeroesComponent>;
+  let heroesComponent: HeroesComponent;
+  const heroServiceMock = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero']);
+  const HEROES = [
+    {id: 1, name: 'me', strength: 0},
+    {id: 2, name: 'superman', strength: 3},
+    {id: 3, name: 'wolverine', strength: 4},
+  ];
+
+  @Component({
+    selector: 'app-hero',
+    template: '<div></div>',
+  })
+  class FakeHeroComponent {
+    @Input()
+    hero: Hero;
+  }
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        HeroesComponent,
+        FakeHeroComponent],
+      providers: [
+        {provide: HeroService, useValue: heroServiceMock}
+      ]
+    });
+    fixture = TestBed.createComponent(HeroesComponent);
+    heroesComponent = fixture.componentInstance;
+  });
+
+  it('create a component', () => {
+    expect(heroesComponent).toBeTruthy('component should be intialized');
+  });
+
+  it('set the heroes correctly from the service', () => {
+    heroServiceMock.getHeroes.and.returnValues(of(HEROES));
+    fixture.detectChanges();
+    expect(heroesComponent.heroes).toEqual(HEROES, 'heroes should be set in the component');
+  });
+
+});
